87f2b8cdc79a57e50e51e12a08a50facf53036fb
James Freeman <jamesfreeman114@gmail.com>
2026-02-04 01:25:01 -0600

    Added bio copy

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -1,9 +1,16 @@
 import "./Bio.css"
+import bioPic from "/src/assets/Bio.png"
+import { bioText } from "../../../database"
 
 export const Bio = () => {
+
+    const copy = bioText()
+
     return(
-    <>
-    <h2 className="bio-heading-text">Here is the Bio</h2>
-    </>
+
+    <section className ="bio-container">
+    <img className="bio-header" src={bioPic} alt="image text" />
+    <p className="bio-text">{copy}</p>
+    </section>
     )
 }
